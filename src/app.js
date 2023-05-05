310b47ee4fa7ba37155f46e015b8b11dfc6907aa
Antonioog <arkanamos1985@gmail.com>
2023-05-05 08:12:14 -0600

    configuracion delproyecto, creamos users model, y la accion de crear nuevo usuario en userscontrollers, creamos la base de datos y hasheamos las contrasenas.

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Server OK.'})
+})
+
+app.listen(9000, () => {
+    console.log(`Server ok 9000`)
+})
